916b2d795c67f2dc1ae4ff903e7d85656a49f21f
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-17 16:54:35 +0600

    Player UI fix

--- src/components/pages/Surah/SurahHead.js
+++ src/components/pages/Surah/SurahHead.js
@@ -4,7 +4,7 @@ import { FiPlayCircle } from "react-icons/fi";
 export const SurahHead = (props) => {
   const { surah } = props;
   return (
-    <div className="flex flex-col md:flex-row md:justify-between md:px-8 items-center p-5 gap-3 bg-gradient-to-tl rounded-2xl text-white from-secondary to-alternateOne mb-5 shadow-xl shadow-alternateOne">
+    <div className="flex flex-col md:flex-row md:justify-between md:px-8 items-center px-5 py-1 gap-3 bg-gradient-to-tl rounded-2xl text-white from-secondary to-alternateOne mb-5 shadow-xl shadow-alternateOne">
       <div className="flex flex-col items-center gap-3 border-b md:border-b-0 p-5">
         <h2 className="font-semibold text-3xl">{surah.name}</h2>
         <h2 className="font-semibold text-3xl">{surah.englishName}</h2>
@@ -19,7 +19,7 @@ export const SurahHead = (props) => {
         بِسْــــــــــــــــــمِ اللهِ الرَّحْمَنِ الرَّحِيْمِ
       </p>
       <FiPlayCircle
-        className="text-4xl cursor-pointer"
+        className="text-4xl cursor-pointer mb-4"
         // onClick={() => audioInstance.play()}
       />
     </div>
