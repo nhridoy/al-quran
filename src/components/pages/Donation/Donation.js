7783ec889fbf6ca3fd8c60ecc44a32f681fca384
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-21 20:35:40 +0600

    Donation Page Design

--- src/components/pages/Donation/Donation.js
+++ src/components/pages/Donation/Donation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Header } from "../../Header/Header";
 import { GoPrimitiveDot } from "react-icons/go";
 // import Swiper core and required modules
@@ -8,9 +8,10 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import "swiper/css/effect-cards";
+
+// import required modules
+import { EffectCards } from "swiper";
 
 export const Donation = () => {
   return (
@@ -18,24 +19,30 @@ export const Donation = () => {
       <Header head="Donation" />
       <div className="grid grid-rows-5">
         <div className="row-span-1 flex items-center justify-center text-lg font-bold">
-          <h2 className=" md:text-2xl">Credits</h2>
+          <h2 className=" md:text-2xl">Donate</h2>
         </div>
-        <div className="row-span-4 bg-secondary text-white p-5 rounded-t-3xl flex flex-col">
-          <div className="bg-red-500">
+        <div className="row-span-4 bg-secondary text-white p-5 rounded-t-3xl flex flex-col items-center gap-4">
+          <p>
+            50% of your donation will go directy to different mosque and islamic
+            education center.
+          </p>
+          <p>Other 50% will be used for server maintanance fee.</p>
+          <div className="">
             <Swiper
               // install Swiper modules
-              modules={[Navigation, Pagination, Scrollbar, A11y]}
-              spaceBetween={50}
-              slidesPerView={3}
-              navigation
-              pagination={{ clickable: true }}
-              scrollbar={{ draggable: true }}
-              onSwiper={(swiper) => console.log(swiper)}
-              onSlideChange={() => console.log("slide change")}
+              effect={"cards"}
+              grabCursor={true}
+              modules={[EffectCards, Navigation, Pagination, Scrollbar, A11y]}
+              className="mySwiper"
             >
               <SwiperSlide>Slide 1</SwiperSlide>
               <SwiperSlide>Slide 2</SwiperSlide>
               <SwiperSlide>Slide 3</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
               <SwiperSlide>Slide 4</SwiperSlide>
             </Swiper>
           </div>
