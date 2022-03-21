29305dd310b0cee88aa1f8419c66207057c500a4
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-21 21:10:23 +0600

    Slider Bug FIx

--- src/components/pages/Donation/Donation.js
+++ src/components/pages/Donation/Donation.js
@@ -41,11 +41,12 @@ export const Donation = () => {
               <SwiperSlide>Slide 2</SwiperSlide>
               <SwiperSlide>Slide 3</SwiperSlide>
               <SwiperSlide>Slide 4</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 5</SwiperSlide>
+              <SwiperSlide>Slide 6</SwiperSlide>
+              <SwiperSlide>Slide 7</SwiperSlide>
+              <SwiperSlide>Slide 8</SwiperSlide>
+              <SwiperSlide>Slide 9</SwiperSlide>
+              <SwiperSlide>Slide 10</SwiperSlide>
             </Swiper>
           </div>
         </div>
