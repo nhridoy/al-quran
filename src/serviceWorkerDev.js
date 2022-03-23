e3c55dfc4954e41126ef9af76d15fa5ed81cb953
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-23 21:49:13 +0600

    SW update test

--- src/serviceWorkerDev.js
+++ src/serviceWorkerDev.js
@@ -1,6 +1,39 @@
+import React from "react";
+import { toast } from "react-toastify";
+
 export const serviceWorkerDev = () => {
-  let serviceWorkerUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
-  navigator.serviceWorker.register(serviceWorkerUrl).then((registration) => {
-    console.log("Service Worker Registered");
-  });
+  // let serviceWorkerUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
+  // navigator.serviceWorker.register(serviceWorkerUrl).then((registration) => {
+  //   console.log("Service Worker Registered");
+  // });
+  // toast.info(`Update available! To update, close all windows and reopen.`, {
+  //   toastId: "appUpdateAvailable", // Prevent duplicate toasts
+  //   onClick: () => window.close(), // Closes windows on click
+  //   autoClose: false, // Prevents toast from auto closing
+  // });
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("./serviceWorker.js")
+      .then((registration) => {
+        registration.update();
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === "installed") {
+              if (navigator.serviceWorker.controller) {
+                alert(
+                  "Update available! To update, close all windows and reopen."
+                );
+              } else {
+                console.log("Content is cached for offline use.");
+              }
+            }
+          };
+        };
+        console.log("Service Worker Registered", registration);
+      })
+      .catch((err) => {
+        console.log("Service Worker Failed to Register", err);
+      });
+  }
 };
