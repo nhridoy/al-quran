eaf61d979440f8eeb497ddb16dd7cdc16d771926
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-23 20:23:09 +0600

    surahlist component dark mode added

--- src/components/pages/SurahList/SurahList.js
+++ src/components/pages/SurahList/SurahList.js
@@ -3,18 +3,18 @@ import { FiOctagon } from "react-icons/fi";
 
 export const SurahList = (props) => {
   return (
-    <div className="py-4 px-3 hover:bg-secondaryLight active:bg-alternateOne rounded-md flex md:flex-col justify-between items-center gap-4 border-b-2 md:border-2 border-alternateOne lg:cursor-pointer">
+    <div className="py-4 px-3 hover:bg-secondaryLight dark:hover:bg-[#191f24] active:bg-alternateOne rounded-md flex md:flex-col justify-between items-center gap-4 border-b-2 md:border-2 border-alternateOne lg:cursor-pointer">
       <div className="flex gap-4">
         <div className="flex gap-3 md:flex-col  items-center">
           <div className="relative">
-            <FiOctagon className="text-primary font-bold text-6xl" />
-            <span className="absolute inset-0 font-semibold grid place-items-center">
+            <FiOctagon className="text-primary dark:text-white font-bold text-6xl" />
+            <span className="absolute inset-0 font-semibold grid place-items-center dark:text-white">
               {props.data.no}
             </span>
           </div>
-          <div className="flex gap-1 flex-col md:items-center">
+          <div className="flex gap-1 flex-col md:items-center dark:text-white">
             <div className="font-semibold">{props.data.enName}</div>
-            <div className="flex flex-col md:flex-row gap-2 text-gray-600 text-xs">
+            <div className="flex flex-col md:flex-row gap-2 text-gray-600 dark:text-gray-400 text-xs">
               <span className="flex gap-1">
                 {props.data.revelationType === "Meccan" ? (
                   <img src="https://img.icons8.com/external-color-outline-adri-ansyah/16/000000/external-islam-islam-and-ramadhan-color-outline-adri-ansyah-8.png" />
@@ -31,7 +31,7 @@ export const SurahList = (props) => {
           </div>
         </div>
       </div>
-      <div className="md:w-full text-right text-sm">
+      <div className="md:w-full text-right text-sm dark:text-white">
         <p>{props.data.name}</p>
         <p>{props.data.enNameTranslation}</p>
         <p>{props.data.bnNameTranslation}</p>
