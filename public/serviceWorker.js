44cdfca67cc741eec8574a1a2cc22ef7615a9d39
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-23 15:41:40 +0600

    PWA test

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -58,7 +58,19 @@ self.addEventListener("fetch", (event) => {
   if (!navigator.onLine) {
     event.respondWith(
       caches.match(event.request).then((response) => {
-        return response || fetch(event.request.clone());
+        if (response) {
+          // retrieve from cache
+          return response;
+        }
+
+        // if not found in cache, return default offline content (only if this is a navigation request)
+        if (event.request.mode === "navigate") {
+          return caches.match("./");
+        }
+
+        // fetch as normal
+        return fetch(event.request.clone());
+        // return response || fetch(event.request.clone());
       })
     );
   }
