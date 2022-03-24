fe881826798184561f6a30000304de4cd9d78018
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-25 05:29:53 +0600

    SW

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -13,6 +13,7 @@ let cacheList = [
   "https://img.icons8.com/external-color-outline-adri-ansyah/16/000000/external-islam-islam-and-ramadhan-color-outline-adri-ansyah-8.png",
   "https://img.icons8.com/external-color-outline-adri-ansyah/64/000000/external-islam-islam-and-ramadhan-color-outline-adri-ansyah-8.png",
   "https://img.icons8.com/external-color-outline-adri-ansyah/16/000000/external-islam-islam-and-ramadhan-color-outline-adri-ansyah-13.png",
+  "https://img.icons8.com/external-jumpicon-glyph-ayub-irawan/32/000000/external-_10-ramadan-jumpicon-(glyph)-jumpicon-glyph-ayub-irawan.png ",
 
   // Local React Files
   "/",
